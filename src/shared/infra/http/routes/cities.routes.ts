a8e7e1b628bd3decfb2bab800171a5db3fc88298
GiovannyLucas <giovannylucax@gmail.com>
2021-12-05 18:05:25 -0300

    feat: :sparkles: added route to find the cities passing a state and/or a name

--- src/shared/infra/http/routes/cities.routes.ts
+++ src/shared/infra/http/routes/cities.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { CreateCityController } from '../../../../modules/cities/useCases/createCity/CreateCity.controller';
+import { FindCitiesController } from '../../../../modules/cities/useCases/findCities/FindCities.controller';
 
 const citiesRouter = Router();
 
 citiesRouter.post('/', CreateCityController.handle);
+citiesRouter.get('/', FindCitiesController.handle);
 
 export { citiesRouter };
