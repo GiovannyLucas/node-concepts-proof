c00e136e51f9f703f6478f60656834a796abb90a
GiovannyLucas <giovannylucax@gmail.com>
2021-12-06 17:01:11 -0300

    feat: :sparkles: added route to get a user passing the id

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { CreateClientController } from '../../../../modules/clients/useCases/createClient/CreateClient.controller';
 import { FindClientsController } from '../../../../modules/clients/useCases/findClients/FindClients.controller';
+import { ShowClientByIdController } from '../../../../modules/clients/useCases/showClientById/ShowClientById.controller';
 
 const clientsRouter = Router();
 
 clientsRouter.post('/', CreateClientController.handle);
 clientsRouter.get('/', FindClientsController.handle);
+clientsRouter.get('/:id', ShowClientByIdController.handle);
 
 export { clientsRouter };
