64036fb4e30886fc811dcc6ae4760587f39c7032
GiovannyLucas <giovannylucax@gmail.com>
2021-12-06 17:59:13 -0300

    feat: :sparkles: added route do remove client

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateClientController } from '../../../../modules/clients/useCases/createClient/CreateClient.controller';
 import { FindClientsController } from '../../../../modules/clients/useCases/findClients/FindClients.controller';
+import { RemoveClientController } from '../../../../modules/clients/useCases/removeClient/RemoveClient.controller';
 import { ShowClientByIdController } from '../../../../modules/clients/useCases/showClientById/ShowClientById.controller';
 
 const clientsRouter = Router();
@@ -9,5 +10,6 @@ const clientsRouter = Router();
 clientsRouter.post('/', CreateClientController.handle);
 clientsRouter.get('/', FindClientsController.handle);
 clientsRouter.get('/:id', ShowClientByIdController.handle);
+clientsRouter.delete('/:id', RemoveClientController.handle);
 
 export { clientsRouter };
